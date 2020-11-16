7c1309e42caf429b473b09fe71da1bd8d201251f
Leo Kushnir <kushnir_l@mail.tel-aviv.gov.il>
2020-11-16 11:32:06 +0000

    Add project files.

---
+++ NofeshloadTest/Helper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NofeshloadTest
+{
+    public static class Helper
+    {
+        public static void SP(string spName, string param)
+        {
+            using (SqlConnection conn = new SqlConnection(@"Data Source=10.111.51.8;User ID=db963_ppr;pwd=h#963719;Initial Catalog=db963"))
+            {
+                conn.Open();
+
+                // 1.  create a command object identifying the stored procedure
+                SqlCommand cmd = new SqlCommand("spName", conn);
+
+                // 2. set the command object so it knows to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which will be passed to the stored procedure
+                //cmd.Parameters.Add(new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    // iterate through results, printing each to console
+                    while (rdr.Read())
+                    {
+                        //Console.WriteLine("Product: {0,-35} Total: {1,2}", rdr["ProductName"], rdr["Total"]);
+                    }
+                }
+            }
+        }
+    }
+}
